abcf62a52d651808eeb4cdb08dcfa01764067ae1
river2056 <chen0625tung@gmail.com>
2023-08-07 19:18:00 +0800

    modified
    + completed leetcode 74

--- leetcode/74_search_a_2d_matrix/main.go
+++ leetcode/74_search_a_2d_matrix/main.go
@@ -1,46 +1,24 @@
 package searcha2dmatrix
 
-import "fmt"
-
 func SearchMatrix(matrix [][]int, target int) bool {
-    rows := make([]int, len(matrix))
-    for i, firstValue := range matrix {
-        rows[i] = firstValue[0]
-    }
+	m := len(matrix)
+	n := len(matrix[0])
+	length := m * n
+	left, right := 0, length-1
 
-    var rowIndex, left, right int;
-    if len(matrix) > 1 {
-        left, right = 0, len(rows)
-        for left < right {
-            var mid int = left + (right - left) / 2
-            if rows[mid] == target {
-                return true
-            } else if rows[mid] > target {
-                right = mid - 1
-            } else {
-                left = mid + 1
-            }
-        }
-        rowIndex = left
-    } else if len(matrix) == 1 {
-        rowIndex = 0
-    }
-
-    if rowIndex < len(matrix) {
-        left, right = 0, len(matrix[rowIndex])
-        for left < right {
-            var mid int = left + (right - left) / 2
-            fmt.Printf("mid: %v\n", mid)
-            fmt.Printf("matrix[rowIndex][mid]: %v\n", matrix[rowIndex][mid])
-            if matrix[rowIndex][mid] == target {
-                return true
-            } else if matrix[rowIndex][mid] > target {
-                right = mid
-            } else {
-                left = mid + 1
-            }
-        }
-    }
+	for left <= right {
+		var mid int = (left + right) / 2
+		var row int = mid / n
+		var col int = mid % n
+		element := matrix[row][col]
+		if element == target {
+			return true
+		} else if element < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
 
 	return false
 }
